b6576cb7d76a06e5e73a2f8839f040b2c1c5d86f
ZiaoChen <466930995@qq.com>
2018-12-27 23:20:37 +0800

    State and Pass functions

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -2,11 +2,45 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 class App extends Component {
+  state = {
+    persons: [
+      { name:'Ziao', age: 28},
+      { name:'Chen', age:34}
+    ]
+  }
+
+  switchNameHandler = (newName) => {
+    // Don't do this this.state.persons[0].name = "Chen Ziao";
+    this.setState({
+      persons: [
+        { name: newName, age: 55},
+        { name:'Chen', age:77}
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: event.target.value, age: 55},
+        { name:'Chen', age:77}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1> Hi, I am a React App </h1>
-        <Person name="Ziao" age="28">This is shit</Person>
+        <button onClick={() => this.switchNameHandler("Tracy")}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler.bind(this,'Li Jiahui')}
+          changed={this.nameChangedHandler}>This is shit</Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}>This is shit</Person>
       </div>
     );
     // return React.createElement('div',{className:'App'},React.createElement('h1',null,"Hi, I am a React App"))
